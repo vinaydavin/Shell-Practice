6923a71f4b09d768a524d0a642d824a2a1029059
vinay davin <vinaydavin@gmail.com>
2026-01-11 17:38:50 +0530

    hello vinay

--- 04.var.sh
+++ 04.var.sh
@@ -3,6 +3,6 @@
 start_time=$(date +%s)
 sleep 10
 end_time=$(date +%s)
-Total_time=$($end_time-$start_time)
+Total_time=$(($end_time-$start_time))
 
 echo "script execution time: $Total_time seconds"
