f2386e304c33742834cfcf326c3a4f3c9bd43d45
vinay davin <vinaydavin@gmail.com>
2026-01-11 17:48:53 +0530

    hello vinay

--- 04.var.sh
+++ 04.var.sh
@@ -1,8 +1,4 @@
 #! /bin/bash
 
-start_time=$(date +%s)
-sleep 10
-end_time=$(date +%s)
-Total_time=$(($end_time-$start_time))
-
-echo "script execution time: $Total_time seconds"
+echo "$@"
+echo "$*"
