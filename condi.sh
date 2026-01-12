08343ccc6c768bfc165403a50583a8efe02410e4
vinay davin <vinaydavin@gmail.com>
2026-01-12 10:53:06 +0530

    1

--- condi.sh
+++ condi.sh
@@ -4,3 +4,11 @@ userid=$(id -u)
 if [ $userid -ne 0 ]; then
   echo "You must be root to run this script."
 fi
+
+dnf install mysql -y
+
+if [$? -ne 0 ]; then
+  echo "MySQL installation failed."
+else
+  echo "MySQL installed successfully."
+fi
