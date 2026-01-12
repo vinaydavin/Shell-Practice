33ca888115ce3ba2fd29d849686e56ecba5cc4aa
vinay davin <vinaydavin@gmail.com>
2026-01-12 15:01:17 +0530

    1

--- hiii.sh
+++ hiii.sh
@@ -1,8 +1,44 @@
-no1=1
-no2=2
-no3=vin
-sum=$(($no1 + $no2+ $no3))
-echo "${sum}"
+#!/bin/bash
 
-abc=("A" "B" "C")
-echo "${abc[1]}"
+userid=$(id -u)
+
+red="\e[31m"
+green="\e[32m"
+yellow="\e[33m"
+reset="\e[0m"
+
+logs_dir="/var/log/shell-script"
+mkdir -p ${logs_dir}
+
+script_name=$(basename "$0" .sh)
+log_file="${logs_dir}/${script_name}.log"
+
+echo "Script started at: $(date)" | tee -a ${log_file}
+
+if [ $userid -ne 0 ]; then
+  echo -e "${yellow}You must be root to run this script.${reset}" | tee -a ${log_file}
+  exit 1
+fi
+
+validate(){
+  if [ $1 -ne 0 ]; then
+    echo -e "$2 installation ${red}FAILED${reset}" | tee -a ${log_file}
+    exit 1
+  else
+    echo -e "${green}$2 installed successfully${reset}" | tee -a ${log_file}
+  fi
+}
+
+install_pkg(){
+  rpm -q $1 &>> ${log_file}
+  if [ $? -ne 0 ]; then
+      dnf install $1 -y &>> ${log_file}
+      rc=$?
+      validate $rc $1
+  else
+      echo -e "${green}$1 already installed${reset}" | tee -a ${log_file}
+  fi
+}
+
+install_pkg net-tools
+install_pkg wget
