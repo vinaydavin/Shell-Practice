89b2141f56f466ea9b8db3ddde5433ed61cbfa66
vinay davin <vinaydavin@gmail.com>
2026-01-12 11:05:51 +0530

    1

--- condi.sh
+++ condi.sh
@@ -3,9 +3,10 @@
 userid=$(id -u)
 if [ $userid -ne 0 ]; then
   echo "You must be root to run this script."
+  exit 1
 fi
 
-dnf install mysql -y
+dnf install net-tools -y
 
 if [$? -ne 0 ]; then
   echo "MySQL installation failed."
