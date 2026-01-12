cc4eb6704dbbc00ef850eadfea6a64191efba58e
vinay davin <vinaydavin@gmail.com>
2026-01-12 10:24:11 +0530

    1

--- condi.sh
+++ condi.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
-a=$1
-
-if [ $a -lt 10 ]; then
-    echo "$a is less than 10"
-else 
-    echo "$a is 10 or greater"
-fi       
-
-# -gt  -eq  -ne 
+read a
+if [ $(($a % 2)) -eq 0 ]; then  
+    echo "Even"
+else
+    echo "Odd"
+fi
