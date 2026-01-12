53c8c34f080bd0975e60754503c07724d84864aa
vinay davin <vinaydavin@gmail.com>
2026-01-12 15:44:13 +0530

    1

--- func.sh
+++ func.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 userid=$(id -u)
-red="\e[31m"
-green="\e[32m"
-yellow="\e[33m"
-reset="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 logs_dir="/var/log/shell-script"
 mkdir -p ${logs_dir}
@@ -15,16 +15,16 @@ log_file="${logs_dir}/${script_name}.log"
 echo "script started at: $(date)" | tee -a ${log_file}
 
 if [ $userid -ne 0 ]; then
-  echo -e "${yellow}You must be root to run this script.${reset}"
+  echo -e "${Y}You must be root to run this script.${N}"
   exit 1
 fi
 
 validate(){
   if [ $1 -ne 0 ]; then
-    echo -e "$2 installation ${red}failed.${reset}" | tee -a ${log_file}
+    echo -e "$2 installation ${R}failed.${N}" | tee -a ${log_file}
     exit 1
   else
-    echo -e "${green}$2 installed successfully.${reset}" | tee -a ${log_file}
+    echo -e "${G}$2 installed successfully.${N}" | tee -a ${log_file}
   fi
 }
 
@@ -34,7 +34,7 @@ if [ $? -ne 0 ]; then
     dnf install net-tools -y &>> ${log_file}
     validate $? "net-tools"
 else
-    echo -e "${green}net-tools already installed.${reset}" | tee -a ${log_file}
+    echo -e "${G}net-tools already installed.${N}" | tee -a ${log_file}
 fi
 
 # Check and install wget
@@ -43,10 +43,17 @@ if [ $? -ne 0 ]; then
     dnf install wget -y &>> ${log_file}
     validate $? "wget"
 else
-    echo -e "${green}wget already installed.${reset}" | tee -a ${log_file}
+    echo -e "${G}wget already installed.${N}" | tee -a ${log_file}
 fi
 
 for package in $@
 do
-    echo "Processing package: $package"
+    echo "package: $package"
+    rpm -q $package &>> ${log_file}
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>> ${log_file}
+        validate $? "$package"
+    else
+        echo -e "${G}$package already installed.${N}" | tee -a ${log_file}
+    fi
 done
