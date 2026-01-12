4e7c8f7d683e5d4fad6f7c425bda1a3c4cc95794
vinay davin <vinaydavin@gmail.com>
2026-01-12 17:37:54 +0530

    1

--- 03.env-var.sh
+++ 03.env-var.sh
@@ -1,3 +1,40 @@
-#! /bin/bash
-
-echo "my name is $name"
+{
+    "ReservationId": "r-04f84a726bb790e7b",
+    "OwnerId": "287636331731",
+    "Groups": [],
+    "Instances": [
+        {
+            "Architecture": "x86_64",
+            "BlockDeviceMappings": [],
+            "ClientToken": "8c52b272-c106-4bbd-9f10-3e2e27747e3c",
+            "EbsOptimized": false,
+            "EnaSupport": true,
+            "Hypervisor": "xen",
+            "NetworkInterfaces": [
+                {
+                    "Attachment": {
+                        "AttachTime": "2026-01-12T10:58:32+00:00",
+                        "AttachmentId": "eni-attach-0d8e27f580c2cfca7",
+                        "DeleteOnTermination": true,
+                        "DeviceIndex": 0,
+                        "Status": "attaching",
+                        "NetworkCardIndex": 0
+                    },
+                    "Description": "",
+                    "Groups": [
+                        {
+                            "GroupId": "sg-05ff4bd44c0bc3ba5",
+                            "GroupName": "default"
+                        }
+                    ],
+                    "Ipv6Addresses": [],
+                    "MacAddress": "0a:ff:d0:d4:0a:03",
+                    "NetworkInterfaceId": "eni-09d659571b0f09617",
+                    "OwnerId": "287636331731",
+                    "PrivateDnsName": "ip-172-31-30-51.ec2.internal",
+                    "PrivateIpAddress": "172.31.30.51",
+                    "PrivateIpAddresses": [
+                        {
+                            "Primary": true,
+                            "PrivateDnsName": "ip-172-31-30-51.ec2.internal",
+                            "PrivateIpAddress": "172.31.30.51"
